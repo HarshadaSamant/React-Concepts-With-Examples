a81fd9e64c307c4645acd26633b5b5c255622dea
Harshada Samant <harshada.samant@neosofttech.com>
2020-06-01 12:46:13 +0530

    first commit

---
+++ src/components/Persons/Person/Person.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Aux from '../../../hoc/Aux';
+import withClass from '../../../hoc/withClass';
+import classes from './Person.css';
+
+class Person extends Component {
+  render() {
+    console.log('[Person.js] rendering...');
+    return (
+      // React has inbuilt Fragment compoennt which works same as Aux
+      <Aux>
+          <p onClick={this.props.click}>
+            I'm {this.props.name} and I am {this.props.age} years old!
+          </p>
+          <p>{this.props.children}</p>
+          <input
+            type="text"
+            onChange={this.props.changed}
+            value={this.props.name}
+          />
+      </Aux>
+    );
+  }
+}
+
+export default withClass(Person, classes.Person);
